7d5563d7e76536cd8ab94721a72d1d308a4f813c
Wuzhixuan <o.s.x@qq.com>
2014-11-20 20:54:09 +0800

    Signed-off-by: Wuzhixuan <o.s.x@qq.com>

---
+++ src/com/a/com.java
@@ -0,0 +1,13 @@
+package com.a;
+
+public class com {
+
+	public com() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("sss");
+	}
+
+}
